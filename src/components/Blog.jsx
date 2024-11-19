1ca3dc8a09876738c97d8a742a51eadf31ebcbad
craigmacharia <cmacharia482@gmail.com>
2024-11-19 11:46:11 +0300

    a few changes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,23 +2,25 @@ import './Blog.css'
 
 function Blog() {
     const posts = [
-        {title: 'Learning React', content: 'React is a powerful JavaScript library for building user interfaces', author: 'John Doe'},
-        {title: 'Getting Started with Vite', content: 'Vite makes development fast and simple', author: 'Jane Smith'}
+        { image: 'https://th.bing.com/th/id/R.51f144b41c898cb0ef9bc8386361dc11?rik=BA97k9dkAUHYzg&pid=ImgRaw&r=0',title: 'Learning coding', content: 'Coding in software engineering', author: 'John Doe' },
+        { image: 'https://image.shutterstock.com/image-vector/concept-blogging-golden-blog-word-260nw-755744683.jpg',title: 'Cyber security', content: 'Cyber Security awereness month', author: 'John Doe' },
     ]
     return (
         <div className='blog'>
-         <h1>Blog Posts</h1>
-         <div className='grid'>
-         {posts.map((post, index) => (
-            <div key={index} className='card'>
-                <h2>{post.title}</h2>
-                <p>{post.content}</p>
-                <p><b>by {post.author}</b></p>
+            <h1>CODDING BLOG</h1>
+            <div className='grid'>
+                {posts.map((post, index) => (
+                    <div key={index} className='card'>
+                        <img src={post.image} alt="" />
+                        <h2>{post.title}</h2>
+                        <p>This is a blog about coding</p>
+                        <p><b>by Craig Macharia</b></p>
+                        <p>Posted on 11th of November,2024 at 11:02 AM</p>
+                    </div>
+                ))}
             </div>
-         ))}
-         </div>
         </div>
-       );
+    );
 }
 
 export default Blog;
